330fd73394a843a8debf99adf25ff9cb02107b72
Sekkay123 <75856121+Sekkay123@users.noreply.github.com>
2021-01-11 13:39:34 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+If you want you see the source code for the bot, you can click [here](https://discord.gg/ST5T4FP) and if you need help, join the [discord server]
---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+
+os.system('pip install -U uvloop')
+
+import uvloop
+import sys
+
+if not os.getenv('DEVICE_ID') and \
+        not os.getenv('ACCOUNT_ID') and \
+        not os.getenv('SECRET'):
+    print("Please paste your device auths into the \".env\" file.\n"
+          "If you're confused, re-watch the tutorial.")
+    sys.exit()
+
+os.system('pip install -U SEKKAYBOT')
+os.system('clear')
+
+import SEKKAYBOT
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+client = SEKKAYBOT.PartyBot(
+    device_id=os.getenv('DEVICE_ID'),
+    account_id=os.getenv('ACCOUNT_ID'),
+    secret=os.getenv('SECRET')
+)
+
+try:
+    client.run()
+except Exception as e:
+    print(e)
+    print("Failed to login, your device auths are probably invalid, please "
+          "try again and make new ones.\nIf you're confused, re-watch the "
+          "tutorial.")

5000f2852a69b8fd36618fcc269982aa2eb1eac9
Sekkay123 <75856121+Sekkay123@users.noreply.github.com>
2021-01-24 18:44:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import SEKKAYBOT
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-client = SEKKAYBOT(
+client = SEKKAYBOT.sekkay(
     device_id=os.getenv('DEVICE_ID'),
     account_id=os.getenv('ACCOUNT_ID'),
     secret=os.getenv('SECRET')

2c9fc1c58fe40d507896061eb86f947736a95818
Sekkay123 <75856121+Sekkay123@users.noreply.github.com>
2021-01-24 18:47:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ import sys
 if not os.getenv('DEVICE_ID') and \
         not os.getenv('ACCOUNT_ID') and \
         not os.getenv('SECRET'):
-    print("Please paste your device auths into the \".env\" file.\n"
-          "If you're confused, re-watch the tutorial.")
+    print("Please paste your device auths into the \".env\" file.\n")
     sys.exit()
 
 os.system('pip install -U SEKKAYBOT')
@@ -30,6 +29,4 @@ try:
     client.run()
 except Exception as e:
     print(e)
-    print("Failed to login, your device auths are probably invalid, please "
-          "try again and make new ones.\nIf you're confused, re-watch the "
-          "tutorial.")
+    print("Can't login because your device auths is probably wrong.")
